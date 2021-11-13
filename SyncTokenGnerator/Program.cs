49eae440d06520e062ada12df6349d08ca88d8fc
Maarten Willebrands <m.willebrands@creates.nl>
2021-11-13 10:56:03 +0100

    Add support to update solr

--- SyncTokenGnerator/Program.cs
+++ SyncTokenGnerator/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Azure.SqlDatabase.ElasticScale.ShardManagement;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SyncTokenGenerator
@@ -13,9 +16,9 @@ namespace SyncTokenGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter the connectionstring to the shardmapmanager");
-
             var shardMapManagerConnectionString = Console.ReadLine();
             Dictionary<string, long?> shardChangeVersions = new Dictionary<string, long?>();
+            string syncToken = "";
 
             try
             {
@@ -56,16 +59,52 @@ namespace SyncTokenGenerator
                 {
                     BinaryFormatter binaryFormatter = new BinaryFormatter();
                     binaryFormatter.Serialize(memoryStream, shardChangeVersions);
+                    syncToken = Convert.ToBase64String(memoryStream.ToArray());
                     Console.WriteLine("The new sync token is:");
-                    Console.WriteLine(Convert.ToBase64String(memoryStream.ToArray()));
+                    Console.WriteLine(syncToken);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                Console.WriteLine("Unable to access the Shard Map Manager database");
-                Console.WriteLine(ex.Message);
+                WriteError("Unable to access the Shard Map Manager database");
+                WriteError(ex.Message);
             }
-                
+
+            Console.WriteLine();
+            Console.WriteLine("Update XDB SOLR Index? (Y/N)");
+            bool updateSolr = Console.ReadLine().ToLowerInvariant() == "y";
+            if (updateSolr)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the SOLR XDB connectionstring, for ex https://localhost:8983/solr/sitecore_xdb");
+                    var solrConnectionString = Console.ReadLine();
+                    if (!solrConnectionString.EndsWith("/"))
+                    {
+                        solrConnectionString += "/";
+                    }
+                    solrConnectionString += "update?commit=true";
+                    using (var webclient = new WebClient())
+                    {
+                        var postBody = $"[{{ \"id\":\"xdb-index-token\",\"xdbtokenbytes_s\":\"{syncToken}\"}}]";
+                        webclient.Headers.Add("content-type", "application/json");
+                        var response = webclient.UploadString(solrConnectionString, "post", postBody);
+                        var jsonResponse = !string.IsNullOrEmpty(response) ? JObject.Parse(response) : null; ;
+                        if (!int.TryParse((jsonResponse["responseHeader"]?["status"] as JValue)?.Value?.ToString(), out var status) || status != 0)
+                        {
+                            WriteError("Invalid response recieved from SOLR");
+                            WriteError(jsonResponse?.ToString());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    WriteError("Unable to update Solr");
+                    WriteError(ex.Message);
+                }
+            }
+
+            Console.WriteLine("Done, press any key to exit");
             Console.ReadLine();
         }
 
@@ -83,6 +122,14 @@ namespace SyncTokenGenerator
                 result.Password = source.Password;
             }
             return result.ToString();
+        }
+
+        static void WriteError(string message)
+        {
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine(message);
+            Console.ForegroundColor = originalColor;
         }
     }
 }
